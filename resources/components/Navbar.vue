3e3c66769f4ca53f97758cc85d5bea0a8a1e32f9
Catif <bradley.barbier@outlook.fr>
2023-02-03 15:56:22 +0100

    Restart github

--- resources/components/Navbar.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <router-link to="/">Home</router-link>
-  <router-link to="/test">Test</router-link>
-</template>
--- resources/views/Test.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Bonjour monde!</h1>
-    <p>Je suis un composant Vue.js nommé <b>Test</b></p>
-  </div>
-</template>
