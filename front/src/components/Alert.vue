e52800d6241ea5ebf4a97c1bd0c79d932be02491
ugo zanzi <ugo.zanzi@gmail.com>
2023-02-07 14:25:44 +0100

    Creation composant Alert

---
+++ front/src/components/Alert.vue
@@ -0,0 +1,16 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+  button{
+    border-radius: 1.125rem;
+    padding: 6px 12px;
+    margin: 12px;
+    background: #ff2d2d;
+    border: none;
+    color: white;
+  }
+</style>
