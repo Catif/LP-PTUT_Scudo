f157d3718097edcf73935130351e6d80e473ec3b
ugo zanzi <ugo.zanzi@gmail.com>
2023-02-07 13:39:51 +0100

    Creation du composant FloatingAppButton

---
+++ front/src/components/FloatingAppButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button>
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+    button{
+        border-radius: 1.125rem;
+        padding: 12px;
+        height: 40px;
+        background: rgb(83, 83, 255);
+        border: none;
+        color: white;
+    }
+</style>
