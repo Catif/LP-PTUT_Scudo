effb7e5765fdf5cbb2d02b12f85a25c07997d130
lecyp12 <contact@cyprien-cotinaut.com>
2023-02-07 19:13:37 +0100

    convertion px to rem

--- front/src/components/Text.vue
+++ front/src/components/Text.vue
@@ -1,12 +1,15 @@
 <template>
   <p>
-
+    <slot></slot>
   </p>
 </template>
 
 <style scoped>
-  p{
-    font-size: 16px;
-    line-height: 20px;
-  }
+p {
+  line-height: 1.25rem;
+
+  font-size: 1rem;
+
+  margin: .75rem;
+}
 </style>
