4eb349f3cc6aa147c7f18591720942b1bda70e38
ugo zanzi <ugo.zanzi@gmail.com>
2023-02-07 14:14:22 +0100

    Creation du composant Button

---
+++ front/src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+    <button>
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+    button{
+        border-radius: 1.125rem;
+        padding: 6px 12px;
+        margin: 12px;
+        background: rgb(83, 83, 255);
+        border: none;
+        color: white;
+    }
+</style>
