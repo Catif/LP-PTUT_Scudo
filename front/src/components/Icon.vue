595ccd94001ef1eb5c9f629ac937c98a581a08b5
lecyp12 <contact@cyprien-cotinaut.com>
2023-02-07 20:17:47 +0100

    création icon

---
+++ front/src/components/Icon.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+const props = defineProps(['active'])
+
+</script>
+<template>
+  <span v-bind:class="{ 'material-symbols-rounded': true, filled: props['active'] }">
+    <slot></slot>
+  </span>
+</template>
+
+<style scoped>
+span {
+  line-height: 1.25rem;
+
+  font-size: 1.5rem;
+
+  font-variation-settings:
+    'FILL' 0,
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 48;
+
+  margin: .75rem;
+}
+
+span.filled {
+  font-variation-settings:
+    'FILL' 1;
+}
+</style>
