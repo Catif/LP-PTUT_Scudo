debdc25708d2ea7aa0d65595a940434800539244
ugo zanzi <ugo.zanzi@gmail.com>
2023-02-08 17:26:22 +0100

    add component groupeCard

---
+++ front/src/components/GroupeCard.vue
@@ -0,0 +1,24 @@
+<script setup>
+import card from '@/components/Card.vue';
+import image from '@/components/Image.vue';
+import texte from '@/components/Text.vue';
+import icon from '@/components/Icon.vue';
+</script>
+
+<template>
+    <card>
+        <image>
+            
+        </image>
+        <texte>
+            
+        </texte>
+        <icon>
+
+        </icon>
+    </card>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
