cb954508b650cea1c90c46d5e36876e8efef5fd1
lecyp12 <contact@cyprien-cotinaut.com>
2023-02-08 14:55:34 +0100

    retirer ligne de test

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,14 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import Navbar from "@/components/Navbar.vue";
-import Test from "@/components/ScudoTheming/Button.vue";
 </script>
 
 <template>
   <header>
     <Navbar />
   </header>
-  <Test>oui</Test>
 
   <RouterView class="container" />
 </template>
