8cc1d62ea18e17c7ca3c046e1ff18b6f5545ba25
lecyp12 <contact@cyprien-cotinaut.com>
2023-02-08 16:03:38 +0100

    création clickable card

---
+++ front/src/components/ScudoTheming/ClickableCard.vue
@@ -0,0 +1,22 @@
+<script  setup>
+import Card from "./Card.vue";
+
+</script>
+
+<template>
+  <Card>
+    <slot></slot>
+  </Card>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/colors";
+
+div:hover {
+  background: $neutral-color-95;
+}
+
+div:active {
+  background: $neutral-color-90;
+}
+</style>
