b096a40acdc5ef6b093f75d587189a74329fd18d
lecyp12 <contact@cyprien-cotinaut.com>
2023-02-08 09:01:47 +0100

    création image

---
+++ front/src/components/Image.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+const props = defineProps(['src', 'alt'])
+
+</script>
+<template>
+  <img :src="props['src']" :alt="props['alt']">
+</template>
+
+<style scoped>
+img {
+  vertical-align: bottom;
+}
+</style>
