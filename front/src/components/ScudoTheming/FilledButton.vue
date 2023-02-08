0d9b4a3d2d5e54eb4b0b074c61007e5817399c6b
lecyp12 <contact@cyprien-cotinaut.com>
2023-02-08 15:48:58 +0100

    heritage

--- front/src/components/ScudoTheming/FilledButton.vue
+++ front/src/components/ScudoTheming/FilledButton.vue
@@ -1,35 +1,23 @@
+<script setup>
+import Button from './Button.vue';
+
+</script>
+
 <template>
-    <button>
+    <Button>
         <slot></slot>
-    </button>
+    </Button>
 </template>
 
 <style lang="scss" scoped>
 @import "@/assets/scss/colors";
 
 button {
-    line-height: 1.5rem;
-
-    font-size: 1rem;
-
-    margin: .75rem;
-    padding: .375rem .75rem;
-
-    border-radius: 1.125rem;
-    border: none;
-
     background: $main-color-40;
-    color: white;
-
-    cursor: pointer;
-
-    transition: background ease-out 150ms;
 }
 
 button:hover {
     background: $main-color-50;
-
-    transition: none;
 }
 
 button:active {
