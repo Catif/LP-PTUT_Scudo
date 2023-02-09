122348251a70cd191e9ab90f558363525b725a96
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-02-09 11:39:21 +0100

    Mobile device

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -1,22 +1,48 @@
+<script setup>
+import Icon from "./ScudoTheming/Icon.vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+console.log(route.path);
+
+const listNavMobile = [
+  { name: "Home", path: "/", icon: "home" },
+  { name: "Upload", path: "/upload", icon: "add_circle" },
+  { name: "Test", path: "/test", icon: "chat_bubble" },
+];
+
+</script>
+
 <template>
   <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/test">Test</router-link>
+    <router-link v-for="el in listNavMobile" :key="el.path" :to="el.path">
+      <Icon :active="route.path == el.path">{{ el.icon }}</Icon>
+    </router-link>
   </nav>
 </template>
 
 <style lang="scss">
+@import "@/assets/scss/colors";
+
 nav {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+
   display: flex;
-  gap: 10px;
-  padding: 1rem;
-  background-color: rgba(255, 255, 255, 0.144);
-  color: #fff;
+  justify-content: space-around;
+  align-items: center;
+
+  height: 3.5rem;
+  background-color: $neutral-color-98;
+  width: 100%;
+
   a {
-    color: rgb(0, 255, 213);
+    color: $neutral-color-10;
     text-decoration: none;
-    &:hover {
-      text-decoration: underline;
+
+    &:visited {
+      color: inherit;
     }
   }
 }
