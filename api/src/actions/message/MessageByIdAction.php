ee6850aee289c9badbf8a4524db72e4282209fd0
Teddy CLEMENT DELS <teddy.clement_dels@outlook.fr>
2023-02-09 16:06:42 +0100

    MessageByIdAction

--- api/src/actions/message/MessageByIdAction.php
+++ api/src/actions/message/MessageByIdAction.php
@@ -6,25 +6,25 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 // SERVICE
-use api\services\ResourceService as ResourceService;
+use api\services\MessageService as MessageService;
 use api\services\utils\FormatterAPI;
 
 //Exception
 use api\errors\exceptions\RessourceNotFoundException as RessourceNotFoundException;
 use Slim\Exception\HttpNotFoundException;
 
-final class ResourceByIdAction
+final class MessageByIdAction
 {
   public function __invoke(Request $rq, Response $rs, array $args): Response {
-    $resourceService = new ResourceService();
+    $messageService = new MessageService();
     try {
-      $resourceById = $resourceService->getResourceByID($args['id']);
+      $messageById = $messageService->getMessageByID($args['id']);
     } catch (RessourceNotFoundException  $e) {
       throw new HttpNotFoundException($rq, $e->getMessage());
     }
     $data = [
-      'Filtre' => 'resourceById',
-      'Result' => $resourceById
+      'Filtre' => 'messageById',
+      'Result' => $messageById
     ];
 
     return FormatterAPI::formatResponse($rq, $rs, $data);
