c3c83fcb09776e0eb18b668c59b9a80fa543679a
ugo zanzi <ugo.zanzi@gmail.com>
2023-02-09 14:58:26 +0100

    nouveau composant ProfilePicture

---
+++ front/src/components/ProfilePicture.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+const props = defineProps(['src', 'alt'])
+
+</script>
+<template>
+<div>
+  <img :src="props['src']" :alt="props['alt']">
+</div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/colors";
+img {
+  aspect-ratio: 1 / 1;
+  border: 2px solid $neutral-color-10;
+  border-radius: 1.125rem;
+  object-fit: cover;
+  vertical-align: bottom;
+}
+</style>
