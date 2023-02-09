a0755fdb18b69fa5f95a9352bfda4a5d32eb8ec6
Teddy CLEMENT DELS <teddy.clement_dels@outlook.fr>
2023-02-09 16:33:35 +0100

    Ajout des Models/Actions/Services (#24)

--- api/public/index.php
+++ api/public/index.php
@@ -1,22 +1,28 @@
 <?php
+declare(strict_types=1);
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
+use Slim\Factory\AppFactory as Appfactory;
+
+
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$conf = parse_ini_file(__DIR__ . '/../conf/db.conf.ini');
 $db = new Illuminate\Database\Capsule\Manager();
-$db->addConnection(parse_ini_file('../conf/db.ini')); /* configuration avec nos paramètres */
+$db->addConnection($conf); /* configuration avec nos paramètres */
 $db->setAsGlobal(); /* rendre la connexion visible dans tout le projet */
 $db->bootEloquent(); /* établir la connexion */
 
 
 $app = AppFactory::create();
-
+$app->addRoutingMiddleware();
 $app->get('/', function (Request $request, Response $response, $args) {
   $response->getBody()->write("Hello world!");
+  
   return $response;
 });
+
 
 $app->run();
---
+++ api/src/models/User.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace api\models;
+class User extends \Illuminate\Database\Eloquent\Model
+{
+
+  protected  $table = 'User';
+  protected  $primaryKey = 'id_user';
+  
+  public $timestamps = true;
+
+
+  public function groups(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+  {
+    return $this->belongsToMany('api\models\Group', 'User_Group', 'id_group', 'id_user');
+  }
+
+  public function follows(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+  {
+    return $this->belongsToMany('api\models\User', 'User_Follow', 'id_user_follow', 'id_user');
+  }
+}
