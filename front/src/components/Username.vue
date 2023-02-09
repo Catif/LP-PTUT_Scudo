161f0403b334c99f4ea9455ed6f90acca7db2d4c
Cyprien COTINAUT <114157370+lecyp12@users.noreply.github.com>
2023-02-09 15:11:14 +0100

    Front username link et small username link (#21)
    * Username
    
    * SmallUsername
    
    * dynamisation link

---
+++ front/src/components/Username.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+const props = defineProps(['to'])
+
+</script>
+
+<template>
+  <router-link :to="props['to']">
+    @<slot></slot>
+  </router-link>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/colors";
+
+a {
+  line-height: 1.25rem;
+
+  font-size: 1rem;
+  text-decoration: none;
+
+  color: $neutral-color-40;
+  transition: color ease-out 150ms;
+}
+
+a:hover {
+  color: $neutral-color-50;
+
+  transition: color ease-out 150ms;
+}
+
+a:active {
+  color: $neutral-color-60;
+
+  transition: none;
+}
+</style>
