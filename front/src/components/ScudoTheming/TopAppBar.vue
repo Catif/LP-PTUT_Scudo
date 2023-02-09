5fed5abd63aa17464d47d47e935b9d2f8bec7918
lecyp12 <contact@cyprien-cotinaut.com>
2023-02-09 09:50:51 +0100

    finalisation top app bar

---
+++ front/src/components/ScudoTheming/TopAppBar.vue
@@ -0,0 +1,47 @@
+<script setup>
+import IconButton from './IconButton.vue';
+
+const props = defineProps(['back', 'title'])
+
+</script>
+<template>
+  <header :class="{ backless: !props['back'] }">
+    <section>
+      <IconButton v-if="props['back']">
+        arrow_back
+      </IconButton>
+      <h1>{{ title }}</h1>
+    </section>
+    <section>
+      <slot></slot>
+    </section>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/colors";
+
+header {
+  display: flex;
+  justify-content: space-between;
+
+  border-bottom: 1px solid $neutral-color-80;
+
+  &.backless {
+    padding-left: .75rem
+  }
+}
+
+section {
+  display: flex;
+}
+
+h1 {
+  line-height: 3.5rem;
+
+  font-size: 1.25rem;
+  font-weight: 400;
+
+  margin: 0;
+}
+</style>
