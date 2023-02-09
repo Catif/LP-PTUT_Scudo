43c69cc91ed33aab8f9bb85d1001137966ae46c4
ugo zanzi <ugo.zanzi@gmail.com>
2023-02-09 17:21:33 +0100

    nouveaux composants ResourceCard et ResourceSection

---
+++ front/src/components/ResourceCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+import ClickableCard from '@/components/ScudoTheming/ClickableFilledCard.vue';
+import Image from '@/components/ScudoTheming/Image.vue';
+import Text from '@/components/ScudoTheming/Text.vue';
+import AuthorSection from '@/components/AuthorSection.vue';
+import ResourceSection from '@/components/ResourceSection.vue';
+const props = defineProps(['user', 'title', 'src', 'alt', 'resource'])
+</script>
+
+<template>
+    <ClickableFilledCard>
+        <ResourceSection :src="props['resource'].image" :alt="props['alt']"/>
+        <AuthorSection :user="props['user']" :title="props['resource'].title"/>
+        <Text>
+            {{ props['resource'].description }}
+        </Text>
+    </ClickableFilledCard>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
