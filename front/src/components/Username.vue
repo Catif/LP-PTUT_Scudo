52d4fce837f801a158c76fdab3d2c42f2b41972e
lecyp12 <contact@cyprien-cotinaut.com>
2023-02-09 12:33:26 +0100

    Username

---
+++ front/src/components/Username.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <router-link to="oui">
+    @<slot></slot>
+  </router-link>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/colors";
+
+a {
+  line-height: 1.25rem;
+
+  font-size: 1rem;
+  text-decoration: none;
+
+  color: $neutral-color-40;
+  transition: color ease-out 150ms;
+}
+
+a:hover {
+  color: $neutral-color-50;
+
+  transition: color ease-out 150ms;
+}
+
+a:active {
+  color: $neutral-color-60;
+
+  transition: none;
+}
+</style>
