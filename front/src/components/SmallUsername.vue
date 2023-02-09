741909666b1b67ae09e6c2042f693b7914637a8b
lecyp12 <contact@cyprien-cotinaut.com>
2023-02-09 12:34:13 +0100

    SmallUsername

---
+++ front/src/components/SmallUsername.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <router-link to="oui">
+    @<slot></slot>
+  </router-link>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/colors";
+
+a {
+  line-height: 1rem;
+
+  font-size: .875rem;
+  text-decoration: none;
+
+  color: $neutral-color-40;
+  transition: color ease-out 150ms;
+}
+
+a:hover {
+  color: $neutral-color-50;
+
+  transition: color ease-out 150ms;
+}
+
+a:active {
+  color: $neutral-color-60;
+
+  transition: none;
+}
+</style>
