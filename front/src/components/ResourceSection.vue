14a2af8568e01e4e66522927fad247e7cd1acab4
ugozanzi <91247012+uzanzi@users.noreply.github.com>
2023-02-10 09:37:08 +0100

    Nouveaux composants ResourceCard et ResourceSection (#26)

---
+++ front/src/components/ResourceSection.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ClickableCard from '@/components/ScudoTheming/ClickableCard.vue';
+import Image from '@/components/ScudoTheming/Image.vue';
+import Text from '@/components/ScudoTheming/Text.vue';
+import AuthorSection from '@/components/AuthorSection.vue';
+
+const props = defineProps(['resource', 'alt'])
+</script>
+
+<template>
+    <Image :src="props['resource'].image" :alt="props['alt']"/>
+</template>
