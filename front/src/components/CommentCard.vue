545f715e5581a1fff196d2008a7eb58a588b8734
ugo zanzi <ugo.zanzi@gmail.com>
2023-02-10 10:16:54 +0100

    nouveau composant CommentCard

---
+++ front/src/components/CommentCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Username from "@/components/Username.vue";
+import ProfilePicture from "@/components/ProfilePicture.vue";
+import Text from "@/components/ScudoTheming/Text.vue";
+
+const props = defineProps(['user', 'comment'])
+
+</script>
+
+<template>
+        <section>
+        <ProfilePicture :src="props['user'].image" alt="profil"/>
+        <div>
+            <Text><Username :to="props['user'].username">{{ props['user'].username }}</Username></Text>
+            <Text>{{ props['comment'] }}</Text>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+    section{
+        display: flex;
+        margin: 0.75rem;
+    }
+    img{
+        height: 80px;
+    }
+</style>
