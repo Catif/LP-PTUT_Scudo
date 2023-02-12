fdf3af98b9f27269e77db4436bb04dc520b409d1
lecyp12 <contact@cyprien-cotinaut.com>
2023-02-12 13:42:35 +0100

    modifcation route vers example

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: "/",
       name: "Home",
-      component: () => import("@/views/HomeView.vue"),
+      component: () => import("@/views/ExampleView.vue"),
     },
     {
       path: "/test",
