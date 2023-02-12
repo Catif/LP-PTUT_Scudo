89cd5b3ab893e1580c0a943fc2cc09e4f3256d7c
Catif <bradley.barbier@outlook.fr>
2023-02-12 10:46:18 +0100

    [FIX] file configuration didn't have .conf

--- api/public/index.php
+++ api/public/index.php
@@ -1,15 +1,16 @@
 <?php
+
 declare(strict_types=1);
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory as Appfactory;
 
-use  api\actions as actions; 
+use  api\actions as actions;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$conf = parse_ini_file(__DIR__ . '/../conf/db.conf.ini');
+$conf = parse_ini_file(__DIR__ . '/../conf/db.ini');
 $db = new Illuminate\Database\Capsule\Manager();
 $db->addConnection($conf); /* configuration avec nos paramètres */
 $db->setAsGlobal(); /* rendre la connexion visible dans tout le projet */
@@ -20,7 +21,7 @@ $app = AppFactory::create();
 $app->addRoutingMiddleware();
 $app->get('/', function (Request $request, Response $response, $args) {
   $response->getBody()->write("Hello world!");
-  
+
   return $response;
 });
 
@@ -36,7 +37,7 @@ $app->get('/api/groups', actions\group\GroupAction::class); // ok
 // SELECT BY ID
 
 $app->get('/users/{id}', api\actions\user\UserByIdAction::class);  // ok
-$app->post('/api/post/test', api\actions\user\UserPostAction::class );
+$app->post('/api/post/test', api\actions\user\UserPostAction::class);
 
 
 
