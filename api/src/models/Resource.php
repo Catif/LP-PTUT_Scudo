019efc2cd112955a4ecdbe8f76194eb51ccf53a9
Catif <bradley.barbier@outlook.fr>
2023-02-12 12:40:51 +0100

    Add user's Resources to the response API

--- api/src/models/Resource.php
+++ api/src/models/Resource.php
@@ -5,14 +5,13 @@ namespace api\models;
 class Resource extends \Illuminate\Database\Eloquent\Model
 {
 
-  protected  $table = 'Ressource';
+  protected  $table = 'Resource';
   protected  $primaryKey = 'id_resource';
-  
+
   public $timestamps = true;
 
   public function groups(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
   {
     return $this->belongsToMany('api\models\Group', 'Resource_Group', 'id_group', 'id_resource');
   }
-
 }
