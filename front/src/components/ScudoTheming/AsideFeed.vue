65413f2021e46e6cfb6539746a4c0498c84fa7d7
lecyp12 <contact@cyprien-cotinaut.com>
2023-02-12 13:42:24 +0100

    création asideFeed

---
+++ front/src/components/ScudoTheming/AsideFeed.vue
@@ -0,0 +1,28 @@
+<template>
+  <aside>
+    <div id="container">
+      <slot></slot>
+    </div>
+  </aside>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/media-queries";
+
+aside {
+  display: none;
+  width: $aside-bar-min-width;
+  flex-shrink: 0;
+
+  &>div {
+    position: sticky;
+    // top: 0;
+  }
+}
+
+@media screen and (min-width : calc($navigation-bar-min-width + $content-min-width + $aside-bar-min-width)) {
+  aside {
+    display: block;
+  }
+}
+</style>
