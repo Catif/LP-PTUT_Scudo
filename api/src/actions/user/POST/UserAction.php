6b6051a7b7b0ecf59bf71215b05cd622b2164873
Catif <bradley.barbier@outlook.fr>
2023-02-12 11:16:19 +0100

    Renamed UserPostAction to UserAction

--- api/src/actions/user/POST/UserAction.php
+++ api/src/actions/user/POST/UserAction.php
@@ -10,13 +10,22 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use api\services\UserService as UserService;
 use api\services\utils\FormatterAPI;
 
-final class UserPostAction
+final class UserAction
 {
     public function __invoke(Request $rq, Response $rs, array $args): Response
     {
         $userService = new UserService;
         $body = $rq->getParsedBody();
-        $modelUser = $userService->insertUser($body);
+        try {
+            $modelUser = $userService->insertUser($body);
+        } catch (\Exception $e) {
+            $data = [
+                'error' => $e->getMessage()
+            ];
+            return FormatterAPI::formatResponse($rq, $rs, $data, 400);
+            return $rs;
+        }
+
 
         $rs->getBody()->write(json_encode($modelUser));
 
