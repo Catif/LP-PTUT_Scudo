fd1bd8c88b0c88af2ceac9eba6557f01d8989e48
ugo zanzi <ugo.zanzi@gmail.com>
2023-02-13 14:18:50 +0100

    nouveau composant Description

---
+++ front/src/components/Description.vue
@@ -0,0 +1,47 @@
+<script setup>
+import Icon from '@/components/ScudoTheming/Icon.vue';
+import Text from '@/components/ScudoTheming/Text.vue';
+import {ref} from "vue";
+
+const props = defineProps(['description'])
+
+var descriptionHidden = ref(false)
+function toggle(){
+    this.descriptionHidden = !this.descriptionHidden
+}
+</script>
+
+<template>
+<div>
+    <template v-if="!descriptionHidden">
+    <button @click="toggle()">
+            <Text class="flex"><div class="text">{{ props['description'] }}</div><Icon>chevron_right</Icon></Text>
+    </button>
+    </template>
+    <template v-else>
+        <div>
+            <Text>{{ props['description'] }}</Text>
+        </div>
+    </template>
+</div>
+</template>
+
+<style lang="scss" scoped>
+    button{
+        width: 500px; // a changer plus tard (Mettre 100% pour la card)
+        background: white;
+        border: none;
+        margin-top: 1.5rem;
+        margin-bottom: 1.5rem;
+        white-space:nowrap;
+        cursor: pointer;
+        .text{
+            overflow:hidden; 
+            white-space:nowrap; 
+            text-overflow: ellipsis;
+        }
+        .flex{
+            display: flex;
+        }
+    }
+</style>
