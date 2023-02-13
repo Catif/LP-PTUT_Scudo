c2b7637571246a45c5aff10afb04b1c291939664
lecyp12 <contact@cyprien-cotinaut.com>
2023-02-13 15:56:41 +0100

    Rennomage GroupCard to SmallGroupCard

---
+++ front/src/components/SmallGroupCard.vue
@@ -0,0 +1,37 @@
+<script setup>
+import Card from '@/components/ScudoTheming/ClickableFilledCard.vue';
+import Image from '@/components/ScudoTheming/Image.vue';
+import Title from '@/components/ScudoTheming/Title.vue';
+import Icon from '@/components/ScudoTheming/Icon.vue';
+
+
+import { useUtils } from "@/stores/utils";
+const utils = useUtils();
+
+const props = defineProps(['group'])
+
+</script>
+
+<template>
+    <Card>
+        <Image :src="props['group'].image" :alt="'Photo de présentation de ' + props['group'].name" />
+        <section>
+            <Title>
+                {{ props['group'].name }}
+            </Title>
+            <Title>
+                {{ utils.formatNumber(group.followers, 2) }}
+                <Icon>
+                    groups
+                </Icon>
+            </Title>
+        </section>
+    </Card>
+</template>
+
+<style lang="scss" scoped>
+section {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
