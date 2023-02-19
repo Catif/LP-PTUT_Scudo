eb585df262f06724a3cd26b59dad9cbf6dc03753
Catif <bradley.barbier@outlook.fr>
2023-02-19 13:29:21 +0100

    Create Files and Routes

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,39 +1,61 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: () => import("@/views/TestView.vue"),
-    },
-    {
-      path: "/settings",
-      name: "settings",
-      component: () => import("@/views/ExampleView.vue"),
-    },
-    {
-      path: "/register",
-      name: "register",
-      component: () => import("@/views/RegisterView.vue"),
-    },
-    {
-      path: "/record",
-      name: "record",
-      component: () => import("@/views/RecordView.vue"),
-    },
-    {
-      path: "/resource/:id",
-      name: "resource",
-      component: () => import("@/views/ResourceView.vue"),
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: () => import("@/views/LoginView.vue"),
-    },
-  ],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: () => import("@/views/TestView.vue"),
+		},
+		{
+			path: "/settings",
+			name: "settings",
+			component: () => import("@/views/ExampleView.vue"),
+		},
+		{
+			path: "/register",
+			name: "register",
+			component: () => import("@/views/RegisterView.vue"),
+		},
+		{
+			path: "/record",
+			name: "record",
+			component: () => import("@/views/RecordView.vue"),
+		},
+		{
+			path: "/resource/:id",
+			name: "resource",
+			component: () => import("@/views/ResourceView.vue"),
+		},
+		{
+			path: "/login",
+			name: "login",
+			component: () => import("@/views/LoginView.vue"),
+		},
+		{
+			path: "/group",
+			name: "group",
+			redirect: "/group/create",
+			children: [
+				{
+					path: "/:id",
+					name: "groupByID",
+					component: () => import("@/views/group/GroupView.vue"),
+				},
+				{
+					path: "/create",
+					name: "groupCreate",
+					component: () => import("@/views/group/GroupCreateView.vue"),
+				},
+				{
+					path: "/:id/edit",
+					name: "groupEdit",
+					component: () => import("@/views/group/GroupEditView.vue"),
+				},
+			],
+		},
+	],
 });
 
 export default router;
