f43b2f69bf57a423d3ddbd4989d32a7b3e058803
Christopher JUE <78968379+JUEChristopher@users.noreply.github.com>
2023-02-19 16:11:59 +0100

    Update database and demo version + removing data generation script from index.php

--- api/public/index.php
+++ api/public/index.php
@@ -2,11 +2,9 @@
 
 declare(strict_types=1);
 
-use Faker\Factory;
 use api\actions as actions;
 use api\services\utils\FormatterAPI;
 
-use api\services\utils\FakerGenerator;
 use Slim\Factory\AppFactory as Appfactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -100,36 +98,4 @@ $app->post('/api/group', actions\group\POST\GroupAction::class); // ok
 $app->run();
 
 
-// Faker
-$fakerFactory = Factory::create();
-$fakerGenerator = new FakerGenerator($fakerFactory);
-
-// Création des users
-
-echo "<br/><br/>================= LOADING DATA GENERATION =================<br/>";
-for($u = 0; $u < 25; $u++){
-  $user = $fakerGenerator->FakeUser();
-  for($ur = 0; $ur < 5; $ur++){
-    $resource = $fakerGenerator->FakeResource($user);
-    if (isset($resource)){
-      for($j = 0; $j < 2; $j++){
-        $fakerGenerator->FakeComment($user, $resource);
-      }
-    }
-  }
-  for($g = 0; $g < 2; $g++){
-    $group = $fakerGenerator->FakeGroup($user);
-    for($gr = 0; $gr < 5; $gr++){
-      $groupResource = $fakerGenerator->FakeGroupResource($user, $group);
-      if (isset($groupResource)){
-        for($z = 0; $z < 2; $z++){
-          $fakerGenerator->FakeComment($user, $groupResource);
-        }
-      }
-    }
-  }
-  if($u > 4){
-    $fakerGenerator->FakeFollow($user, $u);
-  }
-}
-echo "================= DATA GENERATION DONE =================<br/>";
+require_once(__DIR__ . '/../sql/data_generation.php');
