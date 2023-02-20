d11edb070c5862a426c3a6cfa000726a22f0339b
Catif <bradley.barbier@outlook.fr>
2023-02-20 11:48:18 +0100

    [FIX] move all code in try catch

--- api/src/actions/group/GET/GroupByIdAction.php
+++ api/src/actions/group/GET/GroupByIdAction.php
@@ -22,33 +22,34 @@ final class GroupByIdAction
 
     try {
       $groupById = GroupService::getGroupByID($args['id']);
+      $followings = $groupById->users()->where('user_group.id_user', $args['id'])->get();
+
+      foreach ($followings as $following) {
+        $roleUser = $following['role'];
+      }
+
+      $etatFollowing = false;
+      $owner = false;
+      if ($roleUser == 'owner') {
+        $etatFollowing = true;
+        $owner = true;
+      } elseif ($roleUser == 'member') {
+        $etatFollowing = true;
+      }
+
+      $data = [
+        'Filtre' => 'groupById',
+        'count' => count($groupById),
+        'result' => [
+          'group' => FormatterObject::formatGroup($groupById),
+          'following' => $etatFollowing,
+          'owner' => $owner
+        ]
+      ];
+
+      return FormatterAPI::formatResponse($rq, $rs, $data);
     } catch (RessourceNotFoundException  $e) {
       throw new HttpNotFoundException($rq, $e->getMessage());
     }
-    $followings = $groupById->users()->where('User_Group.id_user', $args['id'])->get();
-    foreach ($followings as $following) {
-      $roleUser = $following['role'];
-    }
-
-    $etatFollowing = false;
-    $owner = false;
-    if ($roleUser == 'owner') {
-      $etatFollowing = true;
-      $owner = true;
-    } elseif ($roleUser == 'member') {
-      $etatFollowing = true;
-    }
-
-    $data = [
-      'Filtre' => 'groupById',
-      'count' => count($groupById),
-      'result' => [
-        'group' => FormatterObject::formatGroup($groupById),
-        'following' => $etatFollowing,
-        'owner' => $owner
-      ]
-    ];
-
-    return FormatterAPI::formatResponse($rq, $rs, $data);
   }
 }
