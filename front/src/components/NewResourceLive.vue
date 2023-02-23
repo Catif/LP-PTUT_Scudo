55ac9236f95a029f99ab6fe5dc0c0c981256a932
lecyp12 <contact@cyprien-cotinaut.com>
2023-02-23 12:10:22 +0100

    créationd d'une ressource pour live puis save auto

--- front/src/components/NewResourceLive.vue
+++ front/src/components/NewResourceLive.vue
@@ -3,66 +3,155 @@ import Title from "@/components/ScudoTheming/Title.vue"
 import { reactive, inject } from "vue";
 import IconButton from "./ScudoTheming/IconButton.vue";
 import Input from "./ScudoTheming/Input.vue";
-import FilledButton from "./ScudoTheming/FilledButton.vue";
 import { useRouter, useRoute } from 'vue-router';
 import { useSessionStore } from '@/stores/session.js';
+import Text from '../components/ScudoTheming/Text.vue';
 
 
 const API = inject("api");
 const router = useRouter();
 const route = useRoute();
 const Session = useSessionStore();
+const bus = inject('bus')
 
 var form = reactive({
-  title: '',
-  text: '',
-  type: 'stream',
-  is_private: true,
+  resource: {
+    id: null,
+    title: '',
+    text: '',
+    type: 'text',
+    is_private: 1,
+  }
 })
 
 if (route.params.accessibility == 'public') {
-  form.is_private = false;
+  form.resource.is_private = 0;
 }
-else {
-  form.is_private = true;
+
+function createResource() {
+  API.post('/api/resource', {
+    title: form.resource.title,
+    text: form.resource.text,
+    type: form.resource.type,
+    is_private: form.resource.is_private,
+  }, {
+    headers: {
+      Authorization: Session.data.token
+    }
+  }).then((reponse) => {
+    form.resource = reponse.data.result.resource;
+  }).catch(() => {
+    alert('oups')
+  })
 }
 
 function saveResource() {
-  API.post('/api/resource', {
-    title: form.title,
-    text: form.text,
-    type: form.type,
-    is_private: form.is_private,
+  API.post(`/api/resource/${form.resource.id}`, {
+    title: form.resource.title,
+    text: form.resource.text,
+    is_private: form.resource.is_private,
   }, {
     headers: {
-      'API-Token': Session.data.token
+      Authorization: Session.data.token
     }
-  }).then((result) => {
-    Session.setSession(result.data.token)
-    router.push('/')
+  }).then((reponse) => {
+    form.resource = reponse.data.result.Resource;
+  }).catch(() => {
+    alert('oups');
   })
 }
 
 
 function toggleAccessibility() {
 
-  form.is_private = !form.is_private;
-
-  if (form.is_private) {
+  if (form.resource.is_private) {
     router.push('/record/private');
+    form.resource.is_private = 1;
   } else {
     router.push('/record/public');
+    form.resource.is_private = 0;
   }
+
+  saveResource();
 }
 
-saveResource();
+bus.on('changeOK', function () {
+  saveResource();
+})
+
+
+createResource();
 
 </script>
 <template>
   <IconButton>close</IconButton>
   <Title>Démarrer un enregistrement</Title>
-  <Input name="title" label="Titre" v-model:value="form.title" />
-  <Input name="text" label="Description" v-model:value="form.text" />
-  <FilledButton @click="toggleAccessibility">top</FilledButton>
+  <Input name="title" label="Titre" v-model:value="form.resource.title" />
+  <Input name="text" label="Texte" v-model:value="form.resource.text" />
+  <Text>
+    <label for="role" class="form-control">Partager publiquement</label>
+    <input @click="toggleAccessibility" id="role" name="role" type="checkbox" />
+  </Text>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/assets/scss/colors";
+
+p,
+label {
+  line-height: 1.625rem;
+  cursor: pointer;
+}
+
+p {
+  display: flex;
+  justify-content: space-between;
+}
+
+label {
+  flex-grow: 1;
+}
+
+input[type="checkbox"] {
+  position: relative;
+  width: 1.5em;
+  height: 1.5em;
+  color: $light-text-primary;
+  border: solid 1px $light-border;
+  border-radius: 50%;
+  vertical-align: bottom;
+  appearance: none;
+  outline: 0;
+  cursor: pointer;
+  background: transparent;
+  transition: background 150ms ease-out;
+
+  &::before {
+    position: absolute;
+    content: "";
+    display: block;
+    top: 2px;
+    left: 7px;
+    width: 8px;
+    height: 14px;
+    border-style: solid;
+    border-color: $light-bg-primary;
+    border-width: 0 2px 2px 0;
+    transform: rotate(45deg) translate(-1px);
+    opacity: 0;
+  }
+
+  &:checked {
+    color: $light-text-button-alert;
+    border-color: $light-bg-button;
+    background: $light-bg-button;
+
+    &::before {
+      opacity: 1;
+    }
+
+    ~label::before {
+      clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+    }
+  }
+}
+</style>
