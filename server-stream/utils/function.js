4ce9dd24e972672f969e4f1bcff19aa91d5f85e7
Catif <bradley.barbier@outlook.fr>
2023-02-26 16:40:51 +0100

    Uncomment fetch api

--- server-stream/utils/function.js
+++ server-stream/utils/function.js
@@ -6,72 +6,72 @@ import { statSync, unlinkSync, existsSync, mkdirSync } from "fs";
 //          fonctions
 // ==============================
 export function access(socket, params) {
-  if (!params.sessionid && params.socketEventCustom && !params.msgEvent) {
-    console.log("Client disconnected caused by bad parameters");
-    socket.disconnect();
-  }
+	if (!params.sessionid && params.socketEventCustom && !params.msgEvent) {
+		console.log("Client disconnected caused by bad parameters");
+		socket.disconnect();
+	}
 }
 
 export function initFileVar(file, room) {
-  file.tempFile = null;
-  conf.createFilename(file, room);
+	file.tempFile = null;
+	conf.createFilename(file, room);
 
-  if (!existsSync(conf.folderTemp)) {
-    mkdirSync(conf.folderTemp, { recursive: true });
-  }
+	if (!existsSync(conf.folderTemp)) {
+		mkdirSync(conf.folderTemp, { recursive: true });
+	}
 }
 
 export function getStatFile(file, durationTranscode) {
-  const BYTES_MO = 1024 * 1024;
-  let fileSizeInBytes = statSync(file).size;
-  let fileSizeInMegabytes = fileSizeInBytes / BYTES_MO;
-  let sizeMo = Math.round(fileSizeInMegabytes * 100) / 100;
+	const BYTES_MO = 1024 * 1024;
+	let fileSizeInBytes = statSync(file).size;
+	let fileSizeInMegabytes = fileSizeInBytes / BYTES_MO;
+	let sizeMo = Math.round(fileSizeInMegabytes * 100) / 100;
 
-  ffmpeg.ffprobe(file, (err, metadata) => {
-    let duration = Math.round(100 * metadata.format.duration) / 100;
-    console.log(
-      `Fichier crée : ${file} | durée de la vidéo : ${duration} s  | taille : ${sizeMo} Mo | temps de conversion : ${durationTranscode} s`
-    );
-  });
+	ffmpeg.ffprobe(file, (err, metadata) => {
+		let duration = Math.round(100 * metadata.format.duration) / 100;
+		console.log(
+			`Fichier crée : ${file} | durée de la vidéo : ${duration} s  | taille : ${sizeMo} Mo | temps de conversion : ${durationTranscode} s`
+		);
+	});
 }
 
 export function transcodeVideo(file) {
-  if (!existsSync(conf.folderOutput)) {
-    mkdirSync(conf.folderOutput, { recursive: true });
-  }
+	if (!existsSync(conf.folderOutput)) {
+		mkdirSync(conf.folderOutput, { recursive: true });
+	}
 
-  // Path
-  let pathFileTemp = `${conf.folderTemp}/${file.filename}.tmp`;
-  let pathFileTranscode = `${conf.folderOutput}/${file.filename}.${conf.transcode.extensionFile}`;
+	// Path
+	let pathFileTemp = `${conf.folderTemp}/${file.filename}.tmp`;
+	let pathFileTranscode = `${conf.folderOutput}/${file.filename}.${conf.transcode.extensionFile}`;
 
-  console.log("Début de la conversion");
+	console.log("Début de la conversion");
 
-  // Transcode
-  let startTime = performance.now();
-  ffmpeg()
-    .input(pathFileTemp)
-    .withVideoCodec(conf.transcode.codec)
-    .addOption("-preset", conf.transcode.preset)
-    .addOption("-crf", conf.transcode.quality)
-    .output(pathFileTranscode)
-    .on("end", () => {
-      // Calcul du temps de conversion
-      let endTime = performance.now();
-      let durationTranscode = Math.round(endTime - startTime) / 1000;
+	// Transcode
+	let startTime = performance.now();
+	ffmpeg()
+		.input(pathFileTemp)
+		.withVideoCodec(conf.transcode.codec)
+		.addOption("-preset", conf.transcode.preset)
+		.addOption("-crf", conf.transcode.quality)
+		.output(pathFileTranscode)
+		.on("end", () => {
+			// Calcul du temps de conversion
+			let endTime = performance.now();
+			let durationTranscode = Math.round(endTime - startTime) / 1000;
 
-      console.log("Conversion complete");
+			console.log("Conversion complete");
 
-      // fetch(`${conf.api_url}/api/resource/${file.id}`, {
-      //   method: "POST",
-      //   body: JSON.stringify({ type: "video" }),
-      // });
+			fetch(`${conf.api_url}/api/resource/${file.id}`, {
+				method: "POST",
+				body: JSON.stringify({ type: "video" }),
+			});
 
-      getStatFile(pathFileTranscode, durationTranscode);
-      unlinkSync(pathFileTemp); // Delete file temp
-    })
-    .run();
+			getStatFile(pathFileTranscode, durationTranscode);
+			unlinkSync(pathFileTemp); // Delete file temp
+		})
+		.run();
 }
 
 export default () => {
-  access, initFileVar, transcodeVideo;
+	access, initFileVar, transcodeVideo;
 };
