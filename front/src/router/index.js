8a75527df8597a102910ee803a166eff47911e15
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-02-27 09:40:37 +0100

    Change route homeView in the router

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
 		{
 			path: "/",
 			name: "home",
-			component: () => import("@/views/TestView.vue"),
+			component: () => import("@/views/HomeView.vue"),
 		},
 		{
 			path: "/settings",
