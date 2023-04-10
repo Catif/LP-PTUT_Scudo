21d195667e70a35ba427c6aa0ef95ccfd9a4fd9a
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-04-10 11:43:47 +0200

    prettier

--- api/src/services/utils/FakerGenerator.php
+++ api/src/services/utils/FakerGenerator.php
@@ -9,31 +9,34 @@ use Ramsey\Uuid\Uuid;
 use api\models\Comment;
 use api\models\Resource;
 
-final class FakerGenerator{
+final class FakerGenerator
+{
     private $faker;
 
-    public function __construct($fakerFactory){
+    public function __construct($fakerFactory)
+    {
         $this->faker = $fakerFactory;
     }
 
-    public function FakeUser():User {
+    public function FakeUser(): User
+    {
         $newUser = new User;
 
-        $role = ["user","professional"];
+        $role = ["user", "professional"];
 
-        $newUser->role = $role[rand(0,1)];
+        $newUser->role = $role[rand(0, 1)];
         $newUser->id_user = Uuid::uuid4()->toString();
         $newUser->username = $this->faker->userName();
         $newUser->email = $this->faker->email();
-        $newUser->password = password_hash("1234", PASSWORD_BCRYPT,['cost' => 12]);
+        $newUser->password = password_hash("1234", PASSWORD_BCRYPT, ['cost' => 12]);
         $newUser->biography = $this->faker->paragraph(2);
         $newUser->image = $this->faker->imageUrl(500, 500);
 
-        if($newUser->role === "professional"){
+        if ($newUser->role === "professional") {
             $newUser->fullname = "{$this->faker->firstName()} {$this->faker->lastName()}";
             $newUser->phone = "0650366517";
         }
-    
+
         $newUser->save();
 
         echo "Fake user generated !<br/>";
@@ -41,7 +44,8 @@ final class FakerGenerator{
         return $newUser;
     }
 
-    public function FakeGroup(User $user):Group {
+    public function FakeGroup(User $user): Group
+    {
         $newGroup = new Group;
 
         $newGroup->id_group = Uuid::uuid4()->toString();
@@ -58,68 +62,69 @@ final class FakerGenerator{
         return $newGroup;
     }
 
-    public function FakeResource(User $user):?Resource {
+    public function FakeResource(User $user): ?Resource
+    {
         $newResource = new Resource;
 
-        $type = ["stream","video","text"];
-        
+        $type = ["stream", "video", "text"];
+
         $newResource->id_resource = Uuid::uuid4()->toString();
-        $newResource->type = $type[rand(0,2)];
+        $newResource->type = $type[rand(0, 2)];
         $newResource->title = $this->faker->sentence(3);
         $newResource->text = $this->faker->words(25, true);
         $newResource->longitude = $this->faker->longitude();
         $newResource->latitude = $this->faker->latitude();
         $newResource->is_private = 1;
-        
-        if($newResource->type === "stream"){
+
+        if ($newResource->type === "stream") {
             $newResource->filename = "test";
-        } elseif($newResource->type === "video"){
+        } elseif ($newResource->type === "video") {
             $newResource->filename = "test";
         }
 
-        if($user->role === "user"){
+        if ($user->role === "user") {
             $user->resources()->save($newResource);
             echo "Fake resource generated !<br/>";
             return $newResource;
-        }else{
+        } else {
             echo ("Data unregistered due to User role : Professional<br/>");
             return null;
         }
-        
     }
 
-    public function FakeGroupResource(User $user, Group $group):?Resource {
+    public function FakeGroupResource(User $user, Group $group): ?Resource
+    {
         $newGroupResource = new Resource;
 
-        $type = ["stream","video","text"];
-        
+        $type = ["stream", "video", "text"];
+
         $newGroupResource->id_resource = Uuid::uuid4()->toString();
-        $newGroupResource->type = $type[rand(0,2)];
+        $newGroupResource->type = $type[rand(0, 2)];
         $newGroupResource->title = $this->faker->sentence(3);
         $newGroupResource->text = $this->faker->words(25, true);
         $newGroupResource->longitude = $this->faker->longitude();
         $newGroupResource->latitude = $this->faker->latitude();
         $newGroupResource->is_private = 1;
-        
-        if($newGroupResource->type === "stream"){
+
+        if ($newGroupResource->type === "stream") {
             $newGroupResource->filename = "test";
-        } elseif($newGroupResource->type === "video"){
+        } elseif ($newGroupResource->type === "video") {
             $newGroupResource->filename = "test";
         }
 
-        if($user->role === "user"){
+        if ($user->role === "user") {
             $user->resources()->save($newGroupResource);
             $newGroupResource->groups()->attach($group);
             echo "Fake group resource generated !<br/>";
             return $newGroupResource;
-        }else{
+        } else {
             echo ("[GROUP RESOURCE] Data unregistered due to User role : Professional<br/>");
             return null;
         }
-
     }
 
-    public function FakeComment(User $user, Resource $resource){
+    public function FakeComment(User $user, Resource $resource)
+    {
         $newComment = new Comment;
 
         $newComment->id_comment = Uuid::uuid4()->toString();
@@ -136,13 +141,14 @@ final class FakerGenerator{
         // $resource->comments()->save($newComment);
     }
 
-    public function FakeFollow(User $user, int $max):void {
+    public function FakeFollow(User $user, int $max): void
+    {
         $uuids = [];
         $uuids = User::select(['id_user'])->get();
 
         $userFollowed = $uuids[rand(0, $max)];
 
-        if($user->id_user === $userFollowed){
+        if ($user->id_user === $userFollowed) {
             echo "A user cannot follow himself !<br/>";
         }
 
