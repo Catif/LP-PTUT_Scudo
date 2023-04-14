93880604905a22a18079084986b9aa4774214817
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-04-14 09:11:21 +0200

    Node | Amélioration de la gestion d'erreur (#73)
    * add several test to not crash
    
    * prettier

--- server-stream/events/disconnect.js
+++ server-stream/events/disconnect.js
@@ -1,18 +1,23 @@
 import { transcodeVideo } from "../utils/function.js";
 
 export default (socket, user, file) => {
-	if (user.action == "live") {
-		// disconnect-with is a event handled by RTCMultiConnectionServer to disconnect a specific socket
-		socket.emit("disconnect-with", socket.client.conn.id);
-	}
-	if (user.role == "streamer") {
-		console.log("Fin d'un stream");
+  if (user.action == "live") {
+    // disconnect-with is a event handled by RTCMultiConnectionServer to disconnect a specific socket
+    socket.emit("disconnect-with", socket.client.conn.id);
+  }
 
-		file.tempFile.end();
-		transcodeVideo(user, file);
+  if (user.role == "streamer") {
+    console.log("Fin d'un stream");
 
-		// TO DO : Déconnecter tous les viewers
-	} else if (user.role == "viewer") {
-		console.log("Viewer disconnected");
-	}
+    if (file && file.tempFile) {
+      file.tempFile.end();
+      transcodeVideo(user, file);
+    } else {
+      console.log("Pas de fichier à transcoder");
+    }
+
+    // TO DO : Déconnecter tous les viewers
+  } else if (user.role == "viewer") {
+    console.log("Viewer disconnected");
+  }
 };
